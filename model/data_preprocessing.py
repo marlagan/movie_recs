7b72152b488863ab58162b7316da25523e3a46df
251571 Łaganowska Martyna <251571@edu.p.lodz.pl>
2025-02-13 19:32:37 +0100

    Dataset preperation - Dates, Numercial data processing

--- model/data_preprocessing.py
+++ model/data_preprocessing.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import nltk
 import pandas as pd
 import numpy as np
@@ -7,6 +9,7 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 import string
 from nltk.stem import WordNetLemmatizer
+import json
 
 #collaborative filtering recommendation system using pytorch
 
@@ -48,12 +51,23 @@ print(f"The first ten rows in the ratings dataset: \n {ratings.head()}")
 
 print(f"The first ten rows in the users dataset: \n {users.head()}")
 
-def processing_text_data(data, lemmanization=True, stopwords=True, lowercasing=True, punctuaction=True):
+
+def processing_text_data(data, lemmanization=True, stopwords=True, lowercasing=True, punctuation=True):
+    '''
+
+    :param data: the data we want to process
+    :param lemmanization: boiling down the word to its root
+    :param stopwords: words that are not that important
+    :param lowercasing: changing the size of the letter
+    :param punctuation: the choice whether the punctuation is needed
+    :return: processed data
+    '''
+
     tokenized_data = []
 
-    for row in range(1, len(data)):
+    for row in range(0, len(data)):
         tokenized_row = word_tokenize(data.iloc[row])
-        if punctuaction:
+        if punctuation:
             tokenized_row = [word for word in tokenized_row if word not in string.punctuation]
         if lowercasing:
             tokenized_row = [str.lower(word) for word in tokenized_row]
@@ -67,19 +81,96 @@ def processing_text_data(data, lemmanization=True, stopwords=True, lowercasing=T
 
     return tokenized_data
 
+def normalization(data):
+    '''
+    :param data: the data we want to normalize
+    :return: normalized data
+    '''
+
+    norm_data = deepcopy(data)
+
+    max_value = max(data)
+    min_value = min(data)
+
+    for x in range(len(data)):
+        norm_data.iloc[x] = (data.iloc[x] - min_value) / (max_value - min_value)
+
+    return norm_data.values.tolist()
+
+def process_date(dates):
+    '''
+    :param dates: the date we want to process
+    :return: DataFrame object containing year, month, day
+    '''
+
+    date_row = pd.DataFrame(columns=['year', 'month', 'day'])
+
+    for date in dates:
+        row = len(date_row)
+        year, month, day = date.split('-')
+        date_row.loc[row, 'year'] = year
+        date_row.loc[row, 'month'] = month
+        date_row.loc[row, 'day'] = day
+
+    return date_row
+
+#preparing text data
+
 movies_desc = processing_text_data(movies['overview'])
-print(movies_desc[0])
+movies_title = processing_text_data(movies['title'])
+movies_org_language = processing_text_data(movies['original_language'])
 
-#moies_desc = processing_text_data(movies['de'], lowercasing=True)
+#preapring data containing dates
 
+movies_release_date = process_date(movies['release_date'])
+movies_release_year = movies_release_date['year'].values.tolist()
 
+#preparing numerical data using normalisation
 
+movies_runtime = normalization(movies['runtime'])
+movies_vote_avg = normalization(movies['vote_average'])
 
+print(f'\n How the data looks like after processing: \n ')
+print(f'Description: {movies_desc[0]}')
+print(f'Title: {movies_title[0]}')
+print(f'Original langauge: {movies_org_language[0]}')
+print(f'Release year: {movies_release_year[0]}')
+print(f'Runtime: {movies_runtime[0]}')
+print(f'Vote average: {movies_vote_avg[0]}')
 
+#combinig all movies' processed features into one dataset
+
+movie_dataset = pd.DataFrame({
+    'description': movies_desc,
+    'title': movies_title,
+    'org_language': movies_org_language,
+    'release_date': movies_release_year,
+    'runtime': movies_runtime,
+    'vote_average': movies_vote_avg,
+})
+
+#The first ten rows of the movie dataset
+
+print(movie_dataset.head())
 
 #creating custom DataSet
 
-'''
 class MoviesDataSet():
-    def __init__(self, movies, users, ratings):
-'''
+    def __init__(self, movies, users, ratings, transform=True):
+        self.movies = movies
+        self.users = users
+        self.ratings = ratings
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.movies)
+
+    def __getitem__(self, idx):
+
+        movie_item = self.movies.iloc[idx]
+        user_item = self.users.iloc[idx]
+        rating = self.ratings.iloc[idx]
+
+        return movie_item, user_item, rating
+
+
